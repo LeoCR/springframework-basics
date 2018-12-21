fc23de12c8850ffa3f5ff46df075e662f7749833
Leonardo Aranibar <leo@iMac-de-Leonardo.local>
2018-12-21 11:32:23 -0600

    Pointcut Expressions

--- src/com/aop/aspect/LogginAspect.java
+++ src/com/aop/aspect/LogginAspect.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Component;
 public class LogginAspect {
 	//this is where we add all of our related advices for logging
 	//let's start with an @Before advice
-	@Before("execution(public void addAccount())")
+	//@Before("execution(public void addAccount())")
+	@Before("execution(public void add*())")
+	//@Before("execution(public void com.aop.dao.AccountDAO.addAccount())")
+	
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n=====>>> Executing @Before advice on addAccount()");
 	}
