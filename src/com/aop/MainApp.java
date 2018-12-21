074223094b28049bb43aa4d392e95f70683c01f3
Leonardo Aranibar <leo@iMac-de-Leonardo.local>
2018-12-21 11:20:57 -0600

    Pointcut Expressions - Match

--- src/com/aop/MainApp.java
+++ src/com/aop/MainApp.java
@@ -3,6 +3,7 @@ package com.aop;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.aop.dao.AccountDAO;
+import com.aop.dao.MembershipDAO;
 
 public class MainApp {
 
@@ -11,7 +12,12 @@ public class MainApp {
 		AnnotationConfigApplicationContext context= new AnnotationConfigApplicationContext(Config.class);
 		//get the bean from spring container
 		AccountDAO theAccountDAO=context.getBean("accountDAO",AccountDAO.class);
+		///get memmbership from spring container
 		//call the business method
+		MembershipDAO theMembershipDAO=context.getBean("membershipDAO",MembershipDAO.class);
+		//call the business method buisiiness method
+		theMembershipDAO.addAccount();
+		
 		theAccountDAO.addAccount();
 		//close the context
 		context.close();
