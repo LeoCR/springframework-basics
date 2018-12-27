7f53fb70003b6502c70a7a507b55c16dc70bd3b0
Leonardo Aranibar <leo@iMac-de-Leonardo.local>
2018-12-27 09:25:33 -0600

    spring-aop-read-joinpoint

--- src/com/aop/aspect/LogginAspect.java
+++ src/com/aop/aspect/LogginAspect.java
@@ -1,9 +1,13 @@
 package com.aop.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import com.aop.Account;
 
 @Aspect
 @Component
@@ -11,7 +15,24 @@ import org.springframework.stereotype.Component;
 public class LogginAspect {
 	//this is where we add all of our related advices for logging
 	@Before("com.aop.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
-	public void beforeAddAccountAdvice() {
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
 		System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+		//display the method signature
+		MethodSignature methodSig= (MethodSignature) theJoinPoint.getSignature();
+		
+		System.out.println("Method: "+methodSig);
+		//display  method argmuments
+		//get args
+		Object[] args =theJoinPoint.getArgs();
+		//loop thru args
+		for(Object tempArg:args) {
+			System.out.println(tempArg);
+			if(tempArg instanceof Account) {
+				///dowcast and print Account speciffic stuff
+				Account theAccount=(Account) tempArg;
+				System.out.println("Account name: "+theAccount.getName());
+				System.out.println("Account level: "+theAccount.getLevel());
+			}
+		}
 	}
 }
