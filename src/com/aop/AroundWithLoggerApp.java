e30e804098ca0fd9ef69a9148e8d3df01075e54a
Leonardo Aranibar <leo@iMac-de-Leonardo.local>
2018-12-28 16:38:54 -0600

    spring-aop-around-with-logger

--- src/com/aop/AroundWithLoggerApp.java
+++ src/com/aop/AroundWithLoggerApp.java
@@ -1,24 +1,26 @@
 package com.aop;
 
-import java.util.List;
+
+import java.util.logging.Logger;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.aop.service.TrafficFortuneService;
 
-public class AroundApp {
-
+public class AroundWithLoggerApp {
+	private static Logger myLogger=Logger.getLogger(AroundWithLoggerApp.class.getName());
 	public static void main(String[] args) {
 		//read spring config java class
 		AnnotationConfigApplicationContext context= new AnnotationConfigApplicationContext(Config.class);
 		//get the bean from spring container
 		TrafficFortuneService theFortuneService=context.getBean("trafficFortuneService",TrafficFortuneService.class);
-		System.out.println("\n Main Program: AroundDemoApp");
 		
-		System.out.println("Calling getFortune");
+		myLogger.info("\n Main Program: AroundDemoApp");
+		
+		myLogger.info("Calling getFortune");
 		String data = theFortuneService.getFortune();
-		System.out.println("\nMy fortune is "+data);
-		System.out.println("Finished");
+		myLogger.info("\nMy fortune is "+data);
+		myLogger.info("Finished");
 		//close the context
 		context.close();
 	}
--- src/com/aop/aspect/LogginAspect.java
+++ src/com/aop/aspect/LogginAspect.java
@@ -1,6 +1,7 @@
 package com.aop.aspect;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -20,11 +21,12 @@ import com.aop.Account;
 @Component
 @Order(2)
 public class LogginAspect {
+	private Logger logger=Logger.getLogger(getClass().getName());
 	@Around("execution(* com.aop.service.*.getFortune(..))")
 	public Object aroundGetFortune(ProceedingJoinPoint theProceedingJoinPoint) throws Throwable{
 		//print out method we are advising on
 		String method=theProceedingJoinPoint.getSignature().toShortString();
-		System.out.println("\n=======> Executing @Around on method: "+method);
+		logger.info("\n=======> Executing @Around on method: "+method);
 		//get begin timestamp
 		long begin = System.currentTimeMillis();
 		//now lets execute the method
@@ -33,7 +35,7 @@ public class LogginAspect {
 		long end = System.currentTimeMillis();
 		//compute duration and display it
 		long duration =end-begin;
-		System.out.println("\n=====> Duration :"+duration/1000.0+" seconds");
+		logger.info("\n=====> Duration :"+duration/1000.0+" seconds");
 		return result;
 	}
 	
@@ -41,30 +43,30 @@ public class LogginAspect {
 	public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint) {
 		//print out wich method we are advising on
 		String method=theJoinPoint.getSignature().toShortString();
-		System.out.println("\n=======> Executing @After on method: "+method);
+		logger.info("\n=======> Executing @After on method: "+method);
 				
 	}
 	@AfterThrowing(pointcut="execution(* com.aop.dao.AccountDAO.findAccounts(..))", throwing="theExc")
 	public void afterThrowingFindAccountsAdvice(JoinPoint theJoinPoint,Throwable theExc) {
 		//print out wich method we are advising on
 		String method=theJoinPoint.getSignature().toShortString();
-		System.out.println("\n=======> Executing @AfterThrowing on method: "+method);
+		logger.info("\n=======> Executing @AfterThrowing on method: "+method);
 		
 		//log the exception
-		System.out.println("\n=======> The exception is: "+theExc);
+		logger.info("\n=======> The exception is: "+theExc);
 	}
 	//add new advice for @AfterReturning on the findAccounts method
 	@AfterReturning(pointcut="execution(* com.aop.dao.AccountDAO.findAccounts(..))",returning="result")
 	public void afterReturningFindAccountsAdvice(JoinPoint theJoinPoint,List<Account> result) {
 		//print out wich method we are advising on
 		String method=theJoinPoint.getSignature().toShortString();
-		System.out.println("\n=======> Executing @AfterReturning on method: "+method);
+		logger.info("\n=======> Executing @AfterReturning on method: "+method);
 		//print out the results of the method call
-		System.out.println("\n=======> result is: "+result);
+		logger.info("\n=======> result is: "+result);
 		//let's post-process the data ... let's modify if 
 		//convedrt the account names to uppercase
 		convertAccountNamesToUpperCase(result);
-		System.out.println("\n=======> result is: "+result);
+		logger.info("\n=======> result is: "+result);
 	}
 	private void convertAccountNamesToUpperCase(List<Account> result) {
 		//loop through accounts
@@ -77,22 +79,22 @@ public class LogginAspect {
 	}
 	@Before("com.aop.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
-		System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+		logger.info("\n=====>>> Executing @Before advice on addAccount()");
 		//display the method signature
 		MethodSignature methodSig= (MethodSignature) theJoinPoint.getSignature();
 		
-		System.out.println("Method: "+methodSig);
+		logger.info("Method: "+methodSig);
 		//display  method argmuments
 		//get args
 		Object[] args =theJoinPoint.getArgs();
 		//loop thru args
 		for(Object tempArg:args) {
-			System.out.println(tempArg);
+			logger.info(tempArg.toString());
 			if(tempArg instanceof Account) {
 				///dowcast and print Account speciffic stuff
 				Account theAccount=(Account) tempArg;
-				System.out.println("Account name: "+theAccount.getName());
-				System.out.println("Account level: "+theAccount.getLevel());
+				logger.info("Account name: "+theAccount.getName());
+				logger.info("Account level: "+theAccount.getLevel());
 			}
 		}
 	}
