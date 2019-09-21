f701edcbd0565e7137d3c2bb07d5c63bdbea59a2
LeoCR <leonardoas2007@gmail.com>
2019-09-21 16:50:04 -0600

    Spring boot Dependency Injection Example

---
+++ src/main/java/com/springdependencyinjection/MainApp.java
@@ -0,0 +1,17 @@
+package com.springdependencyinjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MainApp {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context= SpringApplication.run(MainApp.class, args);
+		Alien a = context.getBean(Alien.class);
+		a.show();
+		
+	}
+
+}
